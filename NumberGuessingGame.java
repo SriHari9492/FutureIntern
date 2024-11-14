c5e14ef1eb3d41c123d12abde67a69ffd782f19c
SriHari9492 <harisriv123@gmail.com>
2024-11-14 12:51:37 +0530

    Add files via upload

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -4,11 +4,12 @@ public class NumberGuessingGame{
         
         Random random = new Random();
         int numberToGuess = random.nextInt(100)+1;
+        int maxAttempts =5;
 
         Scanner sc = new Scanner(System.in);
         int userGuess =0;
         int attempts =0;
-         while (userGuess != numberToGuess) {
+         while (userGuess != numberToGuess  && attempts < maxAttempts) {
             System.out.println("Enter your guess:");
             userGuess = sc.nextInt();
             attempts++;
@@ -22,7 +23,11 @@ public class NumberGuessingGame{
             }
             else {
                 System.out.println("Congratulations ! you 've guessed the "+numberToGuess+"in"+attempts+"attempts");
+                break;
             }
+         }
+         if(userGuess != numberToGuess){
+            System.out.println(" you've used all the attempts the correct number :"+ numberToGuess);
          }
          sc.close();
 
